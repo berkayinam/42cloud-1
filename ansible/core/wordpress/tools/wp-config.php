e39dd75e7abe71e72bd75b3f44f2426cbf353dcb
Berkay İnam <berkay_inam@hotmail.com>
2024-11-14 08:15:51 -0500

    Canceled awsEC2, Updated for SehmusServe

--- ansible/core/wordpress/tools/wp-config.php
+++ ansible/core/wordpress/tools/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'dertops');
 define( 'DB_PASSWORD', '123');
 
 /** Database hostname */
-define( 'DB_HOST', 'mariadb' );
+define( 'DB_HOST', 'mariadb:3306' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
